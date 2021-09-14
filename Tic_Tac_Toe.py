7e2fb5b3032073861afcb86a2a2f43493bd65b87
Javier A. Robles <88404168+jrobles32@users.noreply.github.com>
2021-09-14 18:08:47 -0400

    Add Docstrings

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -1,4 +1,14 @@
+import random
+
+
 def display():
+    """
+    Prints a display showing user where numerical inputs will be placed. Returns a list of '-' representing potential
+    placement locations.
+
+    Returns:
+        display_update (list): list of ten elements. Nine are potential placements for 'X' or 'O'.
+    """
     display_fig = ['-', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     display_changes(display_fig)
     display_update = ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-']
@@ -6,13 +16,34 @@ def display():
 
 
 def display_changes(table):
+    """
+    Prints a display showing the current values in index one through nine of the input table.
+
+    Parameters:
+        table (list): a list of ten elements.
+
+    Returns:
+        none
+    """
     print(table[7] + "|" + table[8] + "|" + table[9] +
           "\n" + table[4] + "|" + table[5] + "|" + table[6] +
           "\n" + table[1] + "|" + table[2] + "|" + table[3])
 
 
 def input_logic(table):
+    """
+    Obtains an integer input between 1-9. If integer is represented by '-' in table, it is replaced with an 'X'.
+    Returns an updated table list and prints a display of it.
+
+    Parameters:
+        table (list): a list of ten elements.
+
+    Returns:
+        display_changes(table): print of updated table list.
+    """
     user_input = input("Where on the board do you want to place an X? Input an integer between 1 and 9: ")
+    
+    # Validating user input.
     if user_input.isdigit():
         if 0 < int(user_input) < 10:
             user_input = int(user_input)
@@ -25,7 +56,8 @@ def input_logic(table):
     else:
         print("Please input an integer! Your input was not valid!")
         return input_logic(table)
-
+    
+    # Checking if user input is in use and placing it in appropriate location.
     if table[user_input].isalpha():
         print('Number in use! Input another number!')
         return input_logic(table)
@@ -35,13 +67,25 @@ def input_logic(table):
 
 
 def ai_logic(table):
-    import random
+    """
+    Finds best possible location to replace '-' in table list with an 'O'. Returns an updated table list and prints a
+    display of it.
+
+    Parameters:
+        table (list): a list of ten elements.
+
+    Returns:
+        display_changes(table): print of updated table list.
+    """
     print("It is currently O's turn.")
+    
+    # Finding all empty spaces in list.
     available_moves = []
     for move, index_value in enumerate(table):
         if index_value == '-' and move != 0:
             available_moves.append(move)
 
+    # Determining if its possible to block winning move for 'X' or to win game by placing 'O'.
     for figure in ['O', 'X']:
         for value in available_moves:
             table_copy = table.copy()
@@ -50,6 +94,7 @@ def ai_logic(table):
                 table[value] = 'O'
                 return display_changes(table)
 
+    # Finding available corners and placing 'O' randomly in one of them if they are empty.
     available_corners = []
     for values in available_moves:
         if values in [1, 3, 7, 9]:
@@ -59,10 +104,12 @@ def ai_logic(table):
         table[random_corner] = 'O'
         return display_changes(table)
 
+    # Placing 'O' in center if it is empty.
     if table[5] in available_moves:
         table[5] = 'O'
         return display_changes(table)
 
+    # Placing 'O' in random empty middle outer row/column position.
     available_sides = []
     for values in available_moves:
         if values in [2, 4, 6, 8]:
@@ -74,6 +121,17 @@ def ai_logic(table):
 
 
 def win_logic(table, win_print=False):
+    """
+    Determines if 'X' or 'O' appear three times in a row. Returns a boolean value based upon if a winner is found.
+
+    Parameters:
+        table (list): a list of ten elements.
+        win_print (bool): 'True' to print who won, 'False' otherwise.
+
+    Returns:
+        bool: 'True' if a winner is found, 'False' otherwise.
+    """
+    # All possible winning combinations.
     if table[1] == table[2] == table[3] != '-':
         winner = table[1]
 
@@ -99,7 +157,8 @@ def win_logic(table, win_print=False):
         winner = table[3]
     else:
         return False
-
+    
+    # Checking to see who won the game.
     if winner == 'X':
         if win_print:
             print('X is the Winner! Congratulations!')
@@ -111,44 +170,62 @@ def win_logic(table, win_print=False):
 
 
 def board_full(table):
+    """
+    Finds how many times '-' appears in table list to determine if the game is a draw. Returns a boolean value based on
+    the frequency of '-'.
+
+    Parameters:
+        table (list): a list of ten elements.
+
+    Returns:
+        bool: 'True' if '-' appears only once in table, 'False' otherwise.
+    """
     if table.count('-') > 1:
         return False
+    
     else:
         print('The game is a draw!')
         return True
 
 
-def game():
-    table = display()
+def play_again():
+    """
+    Determines if user wants to play another round. Returns a boolean value based on user input.
+
+    Returns:
+        bool: 'True' if user wants to play again, 'False' otherwise.
+    """
     while True:
-        input_logic(table)
-        if win_logic(table, win_print=True):
-            break
-        if board_full(table):
-            break
-        ai_logic(table)
-        if win_logic(table, win_print=True):
-            break
+        user_input = input("Do you want to play again? (yes/no): ")
+        if user_input.lower() == 'n' or user_input.lower() == 'no':
+            print('Thanks for playing!')
+            return False
+        
+        elif user_input.lower() == 'y' or user_input.lower() == 'yes':
+            return True
+        
+        else:
+            print('Not a valid input! Try Again!')
+            continue
 
 
 def main():
-    import time
-    done = False
-    while not done:
-        user_input = input("Do you want to play the game? (yes/no) ")
-        if user_input.isalpha():
-            if user_input.lower() == 'n' or user_input.lower() == 'no':
-                print('Thanks for playing!')
-                time.sleep(2)
-                done = True
-            elif user_input.lower() == 'y' or user_input.lower() == 'yes':
-                game()
-            else:
-                print('Not a valid input! Try Again!')
-                main()
-        else:
-            print('Not a valid input! Try Again!')
-            main()
+    while True:
+        table = display()
+        while True:
+            input_logic(table)
+            if win_logic(table, win_print=True):
+                break
+
+            if board_full(table):
+                break
+
+            ai_logic(table)
+            if win_logic(table, win_print=True):
+                break
+
+        if not play_again():
+            break
 
 
 if __name__ == "__main__":
