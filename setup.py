7b6a0ae37c8fff162752915a255d72e67c3ba1a0
Javier Robles <javierrobles32@gmail.com>
2022-05-06 18:18:30 -0400

    create package info

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='Simple Games',
+    version='1.0.0',
+    description='package currently contains a sudoku solver and tic-tac-toe',
+    author='Javier Robles',
+    author_email='javierrobles32@gmail.com',
+    packages=find_packages()
+)
